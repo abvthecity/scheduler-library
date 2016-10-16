77616954f98c9e890d7d7d4f025065573519e8d6
Andrew Jiang <andrewji@usc.edu>
2016-10-16 16:56:55 -0700

    version 0.1.71 fixed test

--- test/test1.js
+++ test/test1.js
@@ -1,19 +1,29 @@
 var TROJAN = require('trojan-course-api');
 var normalize = require('./normalize');
-var scheduler = require('./index');
+var scheduler = require('./scheduler-register');
 
 var courses = ['CSCI-201', 'CSCI-270', 'EE-109'];
+var term = 20163;
 
+// Nathan's classes
+courses = ['SWMS-301', 'ACAD-376'];
+
+// all testable entities
 var entities = {};
+
+// get course data for each course
 for (var course of courses) {
   var courseid = course.split('-');
-  TROJAN.course(courseid[0], courseid[1]).then(function (coursedata) {
-    var courseid = Object.keys(coursedata)[0];
-    var coursedata = coursedata[courseid];
+  TROJAN.course(courseid[0], courseid[1], null, term).then(function (coursedata) {
+    coursedata = coursedata[Object.keys(coursedata)[0]];
+    // get combinations
     TROJAN.combinations(coursedata).then(function (combinations) {
+
+      // turn data into a form processable by scheduler-api
       var data = normalize(coursedata, combinations);
       entities[courseid] = data;
 
+      // if entities are all loaded, process it.
       if (Object.keys(entities).length === courses.length) {
         processEntities(entities);
       }
